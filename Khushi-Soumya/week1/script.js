0edbb2713c343747a46ae7d9a19f268ba8230a68
Khushi Soumya <ksoumya246@gmail.com>
2023-07-08 19:00:30 +0530

    first week submission

---
+++ Khushi-Soumya/week1/script.js
@@ -0,0 +1,8 @@
+var button = document.getElementById("demobutton");
+
+button.style.backgroundColor = "blue";
+button.style.color = "white";
+
+button.addEventListener("click", function(){
+    alert(" you clicked button!")
+})
