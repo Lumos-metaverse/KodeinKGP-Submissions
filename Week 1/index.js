72381a38a6b8f75f50c0423cf2e222b952b550dc
Jid1234 <jidchaudhari2005@gmail.com>
2023-07-09 15:09:12 +0530

    week1 submission

---
+++ Week 1/index.js
@@ -0,0 +1,8 @@
+// Get the button Element by its ID
+
+var button=document.getElementById('demobutton');
+
+// Add click event listener to the button
+button.addEventListener('click',function() {
+    alert('Congratulations! You Clicked the Button.');
+});
