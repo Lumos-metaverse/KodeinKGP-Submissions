ddc33efdaa55077cc51f2293a5deaad59d68089a
varshachepuri <106600591+varshachepuri@users.noreply.github.com>
2023-08-07 23:16:14 +0530

    Add files via upload

---
+++ models/quote.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const quoteSchema = new mongoose.Schema({
+    author:{
+        type: String,
+    },
+    quote:{
+        type: String,
+    }
+})
+
+module.exports = mongoose.model('Quote',quoteSchema);
---
+++ routes/quote.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const Quote = require('../models/quote');
+
+router.get('/', async (req,res) => {
+    const quotes = await Quote.find();
+    res.status(200).json(quotes);
+})
+
+router.post('/',async(req,res) => {
+    const quote = new Quote({
+        author : req.body.author,
+        quote : req.body.quote
+    })
+    try{
+        const newQuote = await quote.save();
+        res.status(201).json(newQuote);
+    }catch(err){
+        res.status(400).json({message: err.messge});
+    }
+})
+
+module.exports = router;
